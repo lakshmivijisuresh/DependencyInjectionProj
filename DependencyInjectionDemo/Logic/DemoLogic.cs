eca36091bf7b6360de3275ed89adf6d0263c4ed6
CESLTD\vijayalakshmi.surend <vijayalakshmi.surendran@cesltd.com>
2023-03-14 19:36:59 +0530

    first commit

---
+++ DependencyInjectionDemo/Logic/DemoLogic.cs
@@ -0,0 +1,17 @@
+﻿namespace DependencyInjectionDemo.Logic;
+
+public class DemoLogic : IDemoLogic
+{
+	private readonly ILogger<DemoLogic> logger;
+
+	public int Value1 { get; private set; }
+
+    public int Value2 { get; private set; }
+
+    public DemoLogic(ILogger<DemoLogic> logger)
+    {
+        Value1 = Random.Shared.Next(minValue: 1, maxValue: 1001);
+        Value2 = Random.Shared.Next(minValue: 1, maxValue: 1001);
+		this.logger = logger;
+	}
+}
